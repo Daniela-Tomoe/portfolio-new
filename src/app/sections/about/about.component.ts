13673b35060620958e16cdf5d7c8c986157d6f33
Daniela Tomoe Naka Kawamata <daniela@ezsolucoes.com.br>
2026-05-31 18:41:26 -0300

    att da seção about me

--- src/app/sections/about/about.component.ts
+++ src/app/sections/about/about.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { CardModule } from 'primeng/card';
 import { I18nService } from '../../core/i18n.service';
 import { ScrollRevealDirective } from '../../shared/scroll-reveal.directive';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CardModule, ScrollRevealDirective],
+  imports: [ScrollRevealDirective],
   templateUrl: './about.component.html'
 })
 export class AboutComponent {
   readonly i18n = inject(I18nService);
+  readonly paragraphKeys = ['about.paragraph1', 'about.paragraph2', 'about.paragraph3', 'about.paragraph4'];
 }
