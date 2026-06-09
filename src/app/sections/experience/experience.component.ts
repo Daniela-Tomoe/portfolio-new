2542687045cd094c1daf8a8f2320739f32a8049d
Daniela Tomoe Naka Kawamata <daniela@ezsolucoes.com.br>
2026-06-09 12:00:16 -0300

    feat: att da seção experience

--- src/app/sections/experience/experience.component.ts
+++ src/app/sections/experience/experience.component.ts
@@ -1,28 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { TimelineModule } from 'primeng/timeline';
 import { I18nService } from '../../core/i18n.service';
 import { ScrollRevealDirective } from '../../shared/scroll-reveal.directive';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [TimelineModule, ScrollRevealDirective],
+  imports: [ScrollRevealDirective],
   templateUrl: './experience.component.html'
 })
 export class ExperienceComponent {
   readonly i18n = inject(I18nService);
-  readonly events = [
-    {
-      period: '2024 - Atual',
-      role: 'Desenvolvedor Front-end',
-      company: 'Empresa Atual',
-      summary: 'Construcao de interfaces Angular, integracao com APIs e melhoria continua da experiencia do usuario.'
-    },
-    {
-      period: '2022 - 2024',
-      role: 'Desenvolvedor Web',
-      company: 'Projeto / Consultoria',
-      summary: 'Criacao de landing pages, paineis administrativos e componentes reutilizaveis.'
-    }
-  ];
 }
